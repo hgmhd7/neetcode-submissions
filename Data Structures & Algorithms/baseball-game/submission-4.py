b3a3ccad7865cd375c32740175e8ccf50c2c1216
Howard Mitchell <48733632+hgmhd7@users.noreply.github.com>
2026-06-10 18:32:22 -0500

    Add: baseball-game - submission-4

---
+++ Data Structures & Algorithms/baseball-game/submission-4.py
@@ -0,0 +1,39 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+
+        # Create a result array
+        resultStack = []
+
+        # Total Counter
+        total = 0
+
+        # Loop through the input array and apply the operations
+        for i in range(len(operations)):
+
+            # Create Conditionals to satisf each of the situations when populating the auxilary array
+            if operations[i] == '+':
+                total += resultStack[-1]+resultStack[-2]
+                resultStack.append(resultStack[-1]+resultStack[-2])
+                
+
+            elif operations[i] == 'D':
+                total += 2*resultStack[-1]
+                resultStack.append(2*resultStack[-1])
+                
+
+            elif operations[i] == 'C':
+                total -= resultStack[-1]
+                resultStack.pop()
+                
+
+            else:
+                total += int(operations[i])
+                resultStack.append(int(operations[i]))
+                
+ 
+
+        return total # Time O(n),  Space O(n) for creating the aux array/ Stack, Auxilary space of of O(1) accouting 
+        # for the output.
+
+
+        
