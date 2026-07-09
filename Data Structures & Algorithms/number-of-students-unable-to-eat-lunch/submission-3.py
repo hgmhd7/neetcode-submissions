acc5e06f17d4577a00ffe72e855aad1412bd5397
Howard Mitchell <48733632+hgmhd7@users.noreply.github.com>
2026-07-09 12:18:50 -0500

    Add: number-of-students-unable-to-eat-lunch - submission-3

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-3.py
@@ -0,0 +1,77 @@
+# class Solution:
+#     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+
+#         students1Count = 0
+#         # students0Count = 0
+
+#         for i in students:
+#             if students[i] == 1:
+#                 students1Count += 1
+#             # else:
+#             #     students0Count += 1
+
+#         sandwich1Count = 0
+#         # sandwhich0Count = 0
+
+#         for i in sandwiches:
+#             if sandwiches[i] == 1:
+#                 sandwich1Count += 1
+#             # else:
+#             #     sandwhich0Count += 1
+        
+#         if students1Count == sandwich1Count:
+#             return 0
+#         elif students1Count > sandwich1Count:
+#             return students1Count - sandwich1Count
+#         else:
+#             return sandwich1Count - students1Count
+
+
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+
+        studentsRem = len(students)
+        studentHash = {}
+
+        """SHORTCUT Python Function that replaces the need to count up the hashes in 
+        a explicit Loop.  This essentially just abstracts away all those key value counts """
+        # studentHash = Counter(students)
+
+        for s in students:
+            if s not in studentHash:
+                # Initialize in sstudent hash first if 
+                # Key DOES NOT Exist
+                studentHash[s] = 0
+            studentHash[s] += 1
+        
+        for s in sandwiches:
+            if s in studentHash and studentHash[s] > 0:
+                studentHash[s] -= 1
+                studentsRem -= 1
+            
+            # """Since the ORDER of the Sadwhiches Matters, if there are NO Students Left
+            # to Take the Current Sandwhich, then you can just Exit Loop since it is Impossible to 
+            # Move On to the Next Sandwhich"""
+            else:
+                return studentsRem
+                
+        return studentsRem
+
+
+        """
+        Time: O(n+m)
+        Space: O(1) since the Hash Map we are Using for Storage Only Contains 2 Keys
+        """
+            
+            
+
+
+
+
+        
+
+
+
+        
+        
